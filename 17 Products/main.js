d6b718b16cfaf5dcaa5dfdaaa71de2731c93ddaa
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-21 18:25:45 +0200

    Use HTML template

--- 17 Products/main.js
+++ 17 Products/main.js
@@ -26,19 +26,13 @@ async function loadProducts() {
 }
 
 function addProduct(item) {
-    grid.innerHTML += ` <div class="col">
-                            <div class="card h-100" style="width: 18rem;">
-                                <img class="object-fit-cover" height="230" src="${item.thumbnail}"
-                                    class="card-img-top" alt="Product image">
-                                <div class="card-body">
-                                    <h5 class="card-title">${item.title}</h5>
-                                    <p class="card-text">Rating: ${item.rating} - ${item.price}$</p>
-                                </div>
-                                <div class="card-footer text-center">
-                                    <a href="#" class="btn btn-success px-5">Buy</a>
-                                </div>
-                            </div>
-                        </div>`;
+
+    // Використання шаблону та додавання на сторінку
+    var source = document.getElementById('cardTemplate').innerHTML;
+    var template = Handlebars.compile(source);
+    var cardHtml = template(item);
+
+    grid.innerHTML += cardHtml;
 }
 
 loadBtn.onclick = () => loadProducts();
